e818e6857e65cfeb7cc515972c3779f80c2ed8b1
Shreya Bansal <shreyabansal760@example.com>
2025-11-11 12:01:22 +0530

    Fixed frontend structure and added missing files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
 
+
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 };
 
 export default App;
